b1e71f11e8351e02467373d0d6ea0285945718fe
Rose Kariuki <rosekariuki28@gmail.com>
2017-03-03 04:02:57 +0000

    bookstore-oop

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,37 @@
 class Bookstore(object):
-    pass
-
-
+    def __init__(self, name):
+        self.name = name
+        self.book_list = []
+        
+    def add_book(self, book):
+        self.book_list.append(book)
+        
+    def get_books(self):
+        return self.book_list 
+        
+        
+    def search_books(self, title=None, author=None):
+        search_result = []
+        
+        for book in self.book_list:
+            if title is not None and title.lower() in book.title.lower():
+                search_result.append(book)
+            if author is not None and author == book.author:
+                search_result.append(book)
+        return search_result
+        
+    
 class Author(object):
-    pass
-
-
+    def __init__(self, name, nationality):
+        self.name = name
+        self.nationality = nationality
+        self.books = []
+        
+    def get_books(self):
+        return self.books
+    
 class Book(object):
-    pass
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        author.books.append(self)
